abc967c172c69ef7f246c16387acc91e0a9d8e7e
beatfischer <46295192+beatfischer@users.noreply.github.com>
2019-01-01 19:08:09 +0100

    Remove Decoder comments.

--- TTN_Node_Setup.ino
+++ TTN_Node_Setup.ino
@@ -22,29 +22,6 @@ TheThingsNode *node;
 // Interval between send in seconds
 #define CONFIG_INTERVAL ((uint32_t) 300)
 
-/*
-Decoder payload function
-------------------------
-
-function Decoder(bytes, port) {
-  var decoded = {};
-  var events = {
-    1: 'setup',
-    2: 'interval',
-    3: 'motion',
-    4: 'button'
-  };
-  decoded.event = events[port];
-  decoded.battery = (bytes[0] << 8) + bytes[1];
-  decoded.light = (bytes[2] << 8) + bytes[3];
-  if (bytes[4] & 0x80)
-    decoded.temperature = ((0xffff << 16) + (bytes[4] << 8) + bytes[5]) / 100;
-  else
-    decoded.temperature = ((bytes[4] << 8) + bytes[5]) / 100;
-  return decoded;
-}
-*/
-
 void setup()
 {
   loraSerial.begin(57600);
